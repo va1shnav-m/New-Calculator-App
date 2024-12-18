cc530e7cd050f45c47835d9c2baeef7cd5861384
va1shnav-m <vaishnav.malol@gmail.com>
2024-12-18 11:40:09 +0530

    updation

--- app/src/main/java/com/example/newcalculatorapp/AddActivity.java
+++ app/src/main/java/com/example/newcalculatorapp/AddActivity.java
@@ -1,6 +1,10 @@
 package com.example.newcalculatorapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AddActivity extends AppCompatActivity {
+public class Addition extends AppCompatActivity {
 
+    EditText ed1,ed2;
+    Button b1;
+    String getNum1,getNum2,result;
+    int num1,num2,sum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_addition);
+        b1=(Button) findViewById(R.id.ADD1);
+
+        ed1=(EditText) findViewById(R.id.num1);
+        ed2=(EditText) findViewById(R.id.num2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNum1=ed1.getText().toString();
+                getNum2=ed2.getText().toString();
+                num1=Integer.parseInt(getNum1);
+                num2=Integer.parseInt(getNum2);
+                sum=num1+num2;
+                result=String.valueOf(sum);
+                Toast.makeText(getApplicationContext(),result, Toast.LENGTH_SHORT).show();
+            }
         });
+
+
     }
 }
